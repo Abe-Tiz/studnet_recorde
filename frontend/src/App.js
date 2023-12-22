a7ed1b9348978ee2f561923a600f006c6553857c
Abe-Tiz <106752541+Abe-Tiz@users.noreply.github.com>
2023-12-22 10:22:06 +0300

    add some packeages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
 
 function App() {
-  return <h1 className="text-blue-500 text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-blue-500 text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      
+    </>
+  );
 }
-
 export default App;
